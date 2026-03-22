c78bdda54b888dd55be73c429fb2fce183cba1a8
swa1245 <swarajpatil2123@gmail.com>
2026-03-22 14:33:42 +0530

    change port

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import './workers/convertWorker'; // Import worker to start it processing
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/yrtomp3';
 
 // 1. Middleware
